b29ceee408fa6faeb976758bb38c8264ca54938d
stevenzct <97181113+stevenzct@users.noreply.github.com>
2024-07-28 18:58:57 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form-one></form-one>
+    <form-two></form-two>
+  </div>
+</template>
+
+<script>
+  import formOne from './components/formOne.vue';
+  import formTwo from './components/formTwo.vue';
+
+
+  export default {
+    components: {
+        'form-one': formOne,
+        'form-two': formTwo
+    },
+    data () {
+        return {
+            component: 'form-one'
+        }
+    },
+    methods: {
+        handleSubmit: function(){
+            alert('thanks for submitting');
+        }
+    }
+}
+</script>
+
+
+
+
+
+
+
